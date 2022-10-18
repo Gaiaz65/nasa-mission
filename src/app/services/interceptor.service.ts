781ecd644c643a637799a4ca78670043bdfba63c
Gaiaz65 <gayazapkaev365@gmail.com>
2022-10-18 22:19:29 +0600

    used prettier, deleted unused code

--- src/app/services/interceptor.service.ts
+++ src/app/services/interceptor.service.ts
@@ -10,14 +10,8 @@ import { environment } from 'src/environments/environment';
 export class APIInterceptorService implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
-    // this.headers.append('Access-Control-Allow-Methods');
     const apiReq = req.clone({
       url: `${req.url + environment.api}`,
-      // setHeaders: {
-      //   'Access-Control-Allow-Origin': '*',
-      //   'Cross-Origin-Opener-Policy': '*',
-      //   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
-      // },
     });
 
     return next.handle(apiReq,);
